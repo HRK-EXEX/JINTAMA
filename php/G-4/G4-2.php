bf0d5b29747bacaca994cbe2dcba4a7228b7b1a2
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-16 13:33:22 +0900

    k

--- php/G-4/G4-2.php
+++ php/G-4/G4-2.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".././src/css/G-4/G4-2.css">
+    <link rel="stylesheet" href="././src/css/G-4/G4-2.css">
     <link rel="stylesheet"  href="/src/css/base/dot_font.css" /> 
     <title>管理者画面</title>
 </head>
@@ -29,7 +29,7 @@
         </tr>
         <?php
         $kanri_id = 1;
-        $db->prepare("select * from User?");
+        $db->prepare("select * from User");
         $db->execute([]);
         foreach($db as $user){
             echo'<tr>';
