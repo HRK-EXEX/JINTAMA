4d8338c4c7967394695134ced556e7f43759f7b1
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-17 11:47:51 +0900

    h

--- src/php/G-4/G4-2.php
+++ src/php/G-4/G4-2.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/./src/css/G-4/G4-2.css">
+    <link rel="stylesheet" href="././src/css/G-4/G4-2.css">
     <link rel="stylesheet"  href="kansho/JINTAMA/src/css/base/dot_font.css" /> 
     <title>管理者画面</title>
 </head>
