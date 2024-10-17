3a1d90ed4be2caa5acd0a10ac5887e67124d980b
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-10-17 12:31:33 +0900

    o

--- src/php/G-1/G1-2.php
+++ src/php/G-1/G1-2.php
@@ -1,22 +1,4 @@
-<?php
-require_once '../db.php';
 
-try{
-    $name=$_POST['user_name'] ?? '';
-    $pass=$_POST['password'] ?? '';
-
-    $hashed_pass=password_hash($pass,PASSWORD_DEFAULT);
-
-    $stmt = $db->prepare('INSERT INTO User (user_name, password) VALUES (:name, :pass)');
-    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-    $stmt->bindParam(':pass', $hashed_pass, PDO::PARAM_STR);
-    $stmt->execute();
-
-    header('Location: G1-3.html?name=' .urlencode($name));
-    exit;
-}catch(Exseption $e){
-   echo 'エラー:'. $e->getMessage();
-}
 
 ?>
 <!DOCTYPE html>
--- src/php/G-1/G1-3.php
+++ src/php/G-1/G1-3.php
@@ -1,3 +1,22 @@
+<?php
+require_once '../db.php';
+
+try{
+    $name=$_POST['user_name'] ?? '';
+    $pass=$_POST['password'] ?? '';
+
+    $hashed_pass=password_hash($pass,PASSWORD_DEFAULT);
+
+    $stmt = $db->prepare('INSERT INTO User (user_name, password) VALUES (:name, :pass)');
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':pass', $hashed_pass, PDO::PARAM_STR);
+    $stmt->execute();
+
+    header('Location: G1-3.html?name=' .urlencode($name));
+    exit;
+}catch(Exseption $e){
+   echo 'エラー:'. $e->getMessage();
+}
 <!DOCTYPE html>
 <html lang="ja">
 <head>
