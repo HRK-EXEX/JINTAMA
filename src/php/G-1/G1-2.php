6eae822f8cd1c72f1ebe0a80e4db49518ee25cfe
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-10-17 12:29:26 +0900

    i

--- src/php/G-1/G1-2.php
+++ src/php/G-1/G1-2.php
@@ -38,10 +38,10 @@ try{
             <form action="G1-3.html" method="post">
                 <button class="back dotgothic16-regular" type="button" onclick="location.href='G1-1_title.html'">back</button>
                 <h2 class="h2name">
-                    name <input type="text" name="name" class="textbox" required><br>
+                    name <input type="text" name="user_name" class="textbox" required><br>
                 </h2>
                 <h2 class="h2password">
-                    password <input type="password" name="pass" class="textbox" required><br>
+                    password <input type="password" name="password" class="textbox" required><br>
                 </h2>
         
                 <div class="op_btn "><input class="dotgothic16-regular" type="submit" value="create"></div>
---
+++ src/php/G-1/G1-3.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"  href="/src/css/base/black_window.css" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="/src/css/G-1/G1-3.css" />
+    <link rel="stylesheet"  href="/src/css/base/dot_font.css" /> 
+
+    <title>アカウント作成確認</title>
+</head>
+<body>
+
+    <div class="all">
+<div class="kuro">
+        <button class="back" onclick="location.href='G1-2.html'">back</button>
+        <div class="input-container">
+    <h2 class="h2name">name </h2><span id="name" class="input-name"></span></div>
+    <div class="input-container">
+    <h2 class="h2password">password </h2><span id="pass" class="input-pass"></span></div>
+    
+    
+    <form action="/src/html/G-1/G1-4.html" method="GET">
+        <input type="hidden" name="name" >
+        <input type="hidden" name="pass" >
+        <div class="op_btn "><input type="submit" value="ok"></div>
+    </form>
+</div>  
+</div>
+</div>
+<script>
+     const params = new URLSearchParams(location.search);
+       document.getElementById('name').textContent = params.get('name') 
+        document.getElementById('pass').textContent = params.get('pass')  
+</script>
+</body>
+</html>
