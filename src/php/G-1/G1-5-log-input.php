7bf3c822f7d82a839a2fb0d721f1f4f5a0fa8e71
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-10-22 08:42:32 +0900

    ふしぎなもんだい

--- src/php/G-1/G1-5-log-input.php
+++ src/php/G-1/G1-5-log-input.php
@@ -14,8 +14,8 @@
         <div class="kuro">
             <form action="/kansho/JINTAMA/src/php/G-1/G1-5-log-output.php" method="post">
             <h1 class="log">Login</h1><br>
-            <h2 class="h2name">name<input type="text" name="user_name" id="user_name" class="textbox"></h2><br>
-            <h2 class="h2password">password<input type="password" name="password" id="password" class="textbox"></h2><br>
+            <h2 class="h2name">name<input type="text" name="user_name_form" id="user_name" class="textbox"></h2><br>
+            <h2 class="h2password">password<input type="password" name="password_form" id="password" class="textbox"></h2><br>
             <div class="op_btn">
                 <button type="submit" class="btn1">OK</button><br>
                 <?php if(isset($_GET['hogeA'])): ?>
