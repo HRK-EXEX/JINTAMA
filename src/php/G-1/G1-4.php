7d3e16d62f0e5059155e50946bd0ab1a8e2a404d
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-21 12:19:36 +0900

    j

---
+++ src/php/G-1/G1-4.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="/src/css/base/black_window.css" /> 
+    <link rel="stylesheet"  href="/src/css/base/dot_font.css" /> 
+    <link rel="stylesheet"  href="/src/css/G-1/G1-4.css" />
+    
+    <title>G1-4</title>
+</head>
+<body>
+    <div class="all">
+        <div class="kuro">
+            
+            <h1 class="complete">作成完了</h1><br>
+            <div class="op_btn">
+                <form action="/src/html/G-3/G2-1.html" method="get">
+                <button type="submit" class="btn1">メニュー画面へ</button><br>
+            </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+<!-- require_once '../db.php';
+
+try{
+    $name=$_POST['user_name'] ?? '';
+    $pass=$_POST['password'] ?? '';
+
+    $hashed_pass=password_hash($pass,PASSWORD_DEFAULT);
+
+    $stmt = $db->prepare('INSERT INTO User (user_name, password) VALUES (:name, :pass)');
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':pass', $hashed_pass, PDO::PARAM_STR);
+    $stmt->execute();
+
+    header('Location: G1-3.php?name=' .urlencode($name) . '&pass=' . urlencode($hashed_pass));
+    exit;
+}catch(Exception $e){
+   echo 'エラー:'. $e->getMessage();
+} -->
