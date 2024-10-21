09ebef778717732b1574200ae54b5b8f4415b5d8
odahayato112 <hayato0112.0@icloud.com>
2024-10-21 12:13:27 +0900

    a

--- src/php/G-1/G1-5-log-output.php
+++ src/php/G-1/G1-5-log-output.php
@@ -9,21 +9,21 @@ require '../db.php'; // データベース接続
 try {
     // POSTデータが存在するか確認
 
-    echo "<hr>";
-    var_dump($_POST);
-    echo "<hr>";
-    echo "u_name / ";
-    echo $_POST['user_name'];
-    echo "<hr>";
-    echo "u_name(empty) / ";
-    echo $_POST['user_name'];
-    echo "<hr>";
-    echo "pass / ";
-    echo $_POST['password'];
-    echo "<hr>";
-    echo "pass(empty) / ";
-    echo empty($_POST['password']);
-    echo "<hr>";
+    // echo "<hr>";
+    // var_dump($_POST);
+    // echo "<hr>";
+    // echo "u_name / ";
+    // echo $_POST['user_name'];
+    // echo "<hr>";
+    // echo "u_name(empty) / ";
+    // echo $_POST['user_name'];
+    // echo "<hr>";
+    // echo "pass / ";
+    // echo $_POST['password'];
+    // echo "<hr>";
+    // echo "pass(empty) / ";
+    // echo empty($_POST['password']);
+    // echo "<hr>";
     
     if (strlen($_POST['user_name']) > 0 && strlen($_POST['password']) > 0) {
         // SQL文をプリペアドステートメントで準備
