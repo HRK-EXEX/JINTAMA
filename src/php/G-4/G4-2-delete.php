17a61091308e2e506145e9f59baba07e98747b89
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-21 11:58:01 +0900

    s

--- src/php/G-4/G4-2-delete.php
+++ src/php/G-4/G4-2-delete.php
@@ -4,5 +4,7 @@
 $id=$_POST['userid'];
 $stm = $db->prepare("DELETE FROM `User` WHERE user_id = ?;");
 $stm->execute([$id]);
-header('Location:/kansho/JINTAMA/src/php/G-4/G4-2.php');
+
+// header('Location:/kansho/JINTAMA/src/php/G-4/G4-2.php');
 ?>
+<hr>
