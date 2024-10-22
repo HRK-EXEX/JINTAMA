48185cfff9bd7d782e8f17e6aa11b5fe3f73d2fb
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-22 14:44:27 +0900

    u

--- src/php/G-2/G2-4-teaminsert.php
+++ src/php/G-2/G2-4-teaminsert.php
@@ -9,10 +9,10 @@ if($human==4){
 $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,null,null)");
      $stm->execute([$teamname,$userid]);
 }elseif($human==3){
-    $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,null,0)");
+    $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,null,'9999')");
      $stm->execute([$teamname,$userid]);
 }else{
-    $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,0,0)");
+    $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,'9999','9999')");
      $stm->execute([$teamname,$userid]);
 }
 
--- src/php/G-2/G2-4.php
+++ src/php/G-2/G2-4.php
@@ -1,6 +1,6 @@
 <?php session_start();
     // $userid= $_SESSION['User']['user_id'];
-    $userid = 1;
+    $userid = 2;
 ?>
 <!DOCTYPE html>
 <html lang="en">
