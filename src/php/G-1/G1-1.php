b148bba34cf636a5cd380c005636fb204c1ac861
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-22 14:23:13 +0900

    d

--- src/php/G-1/G1-1.php
+++ src/php/G-1/G1-1.php
@@ -3,21 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/css/G-1/G1-1.css" />
+    <link rel="stylesheet" href="/kansho/JINTAMA/src/css/G-1/G1-1.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
     <title>タイトル画面</title>
     <script src="https://cdn.jsdelivr.net/npm/phaser@v3.85.2/dist/phaser.min.js"></script>
-    <script src="/src/js/G-1/G1-1.js"></script>
+    <script src="/kansho/JINTAMA/src/js/G-1/G1-1.js"></script>
     
 </head>
 <body>
     <div class="title">
-        <img src="/img/JINTAMA.png" alt="タイトル画像" id="title">
+        <img src="/kansho/JINTAMA/img/JINTAMA.png" alt="タイトル画像" id="title">
     </div>
     <div class="kuro" id="show-later">
-        <a href="G1-5.html"><h1>ログインへ</h1></a>
+        <a href="G1-5.php"><h1>ログインへ</h1></a>
     </div>
    
 
