615894460ab3b2313aee1d942cd86933ce01c1eb
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-22 14:15:50 +0900

    d

--- src/php/G-2/G2-4-teaminsert.php
+++ src/php/G-2/G2-4-teaminsert.php
@@ -3,8 +3,10 @@ require '../db.php';
 
 $userid = $_POST['userid'];
 $teamname = $_POST['teamname']
-$human = $_POST['humen'];
+// $human = $_POST['humen'];
 
 $stm = $db->prepare("INSERT INTO `Room`(`room_id`, `room_name`, `room_user1`, `room_user2`, `room_user3`, `room_user4`) VALUES (null,?,?,null,null,null)");
      $stm->execute([$teamname,$userid]);
 
+     header('Location:/kansho/JINTAMA/src/php/G-2/G2-5.php');
+?>
