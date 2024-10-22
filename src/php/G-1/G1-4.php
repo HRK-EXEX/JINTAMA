06e100eb46be83d355e1091920ac19e495b1d926
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-10-22 10:32:57 +0900

    s

--- src/php/G-1/G1-4.php
+++ src/php/G-1/G1-4.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"  href="/src/css/base/black_window.css" /> 
-    <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/base/dot_font.css" " /> 
+    <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/base/dot_font.css"/> 
     <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/G-1/G1-4.css" />
     
     <title>G1-4</title>
