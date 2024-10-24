bc2908e24261d0a764e92e27af34444a9bb4bb50
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-10-24 12:36:41 +0900

    G2-7

---
+++ src/php/G-2/G2-7.php
@@ -0,0 +1,51 @@
+<?php require '../db.php';?>
+<?php
+session_start(); 
+
+
+$newname = $_SESSION['user_name'] ?? '未入力';
+
+     $sql = "UPDATE user SET user_name =:new_name "
+     $stm->execute([$_POST['name'],$hashed_pass]);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/kansho/JINTAMA/src/css/base/black_window.css" />
+    <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/G-2/G2-7.css" />
+    <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/base/dot_font.css" /> 
+    <title>設定画面</title>
+</head>
+<body>
+
+    <div class="kuro">
+        <a href="/src/html/G-2/G2-8.html"><p class="delete">アカウント削除</p></a>
+        <form action="G2-8">
+            <div class="username">
+                ユーザー名 <input type="text"><br>
+            </div>
+            <div class="ranking">
+                ランキングへの情報送信<br>
+            <input type="radio" name="hozon" id="suru" class="radi" /><label for="suru">する？</label>
+           <input type="radio" name="hozon" id="sinai" class="radi" /><label for="sinai">しない？</label>
+        </div>
+        </form>
+        
+    <form action="#">
+        <button type="submit">
+            保存
+        </button>
+    </form>
+
+            
+        <button type="submit" onclick="location.href='/src/html/G-2/G2-1_mainmenu.html'">
+            戻る
+        </button>
+
+</div>
+
+</body>
+</html>
