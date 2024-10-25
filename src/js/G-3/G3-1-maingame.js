239e7ee20113e9c444980d75f495cb0e864ff025
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-10-25 11:05:46 +0900

    とりまプッシュ

--- src/js/G-3/G3-1-maingame.js
+++ src/js/G-3/G3-1-maingame.js
@@ -16,7 +16,12 @@ const tilemapNames = [
     'dirtTile2',
 ]
 
-var cursors
+// 変数名定義
+var カーソル;
+var キー;
+
+// 関数名定義
+var input;
 
 var mapX = 0, mapY = 0, scale = 4, spd = 10;
 
@@ -30,9 +35,6 @@ class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        // // プラグインを参照
-        // GAME.add.plugin(Phaser.Plugin.Tiled);
-
         // /*
         //     ╔════════════════════════╗
         //     ║ ２つ目のマップをロード ║
@@ -46,27 +48,17 @@ class MainScene extends Phaser.Scene {
         this.load.tilemapTiledJSON('map', '/src/js/G-3/map-data/second-map.json')
     }
 
-    // In our create() function, let's first add the background and scale it for our resolution:
     create() {
-        // Let's add our map:
+        // マップを追加
         const map = this.make.tilemap({ key: 'map' })
 
-        // The key matches the name given in the preload() function when we loaded the Tiled JSON.
-        // We also have to add the tileset image to our Phaser map object:
+        // キーは、Tiled JSONをロードしたときにpreload()関数で与えられた名前と一致する必要がある。
+        // また、タイルセット画像をPhaserマップオブジェクトに追加する必要がある。
         const baseTileSet = map.addTilesetImage(tilemapNames[0], 'baseTileImage')
         const dirtTileSet1 = map.addTilesetImage(tilemapNames[1], 'dirtTileImage')
         const dirtTileSet2 = map.addTilesetImage(tilemapNames[2], 'dirtTileImage2')
-        // The first argument of addTilesetImage is the name of the tileset we used in Tiled.
-        // The second argument is the key of the image we loaded in the preload() function.
-
-        // 地面レイヤー作成 DynamicTilemapLayerオブジェクト作成
-        const layerWidth = 64 * 1 * 12
-        const layerHeight = 64 * 1 * 12
-
-        // レイヤーの左上X座標
-        const layerOriginX = (this.game.config.width - layerWidth) / 2
-        // レイヤーの左上Y座標
-        const layerOriginY = (this.game.config.height - layerHeight) / 2
+        // addTilesetImageの第一引数は、Tiledで使ったタイルセットの名前。
+        // 第二引数は、preload()関数で読み込んだ画像のキーである。
 
         const relatedTileSet = [
             baseTileSet,
@@ -92,11 +84,34 @@ class MainScene extends Phaser.Scene {
         }
 
         // 入力のハンドリング
-        cursors = this.input.keyboard.createCursorKeys()
+        カーソル = this.input.keyboard.createCursorKeys()
+        キー = this.input.keyboard.addKeys(
+            {
+                'SPACE': Phaser.Input.Keyboard.KeyCodes.SPACE,
+                'ENTER': Phaser.Input.Keyboard.KeyCodes.ENTER,
+                'BACKSPACE': Phaser.Input.Keyboard.KeyCodes.BACKSPACE,
+                'ESC': Phaser.Input.Keyboard.KeyCodes.ESC,
+            }
+        );
+
+        input = function input() {
+            // ボタン操作
+            var button = (カーソル.left.isDown ? 1 : 0) + 
+                        (カーソル.right.isDown ? 2 : 0) + 
+                        (カーソル.up.isDown ? 4 : 0) + 
+                        (カーソル.down.isDown ? 8 : 0) +
+                        (キー.SPACE.isDown ? 16 : 0) +
+                        (キー.ENTER.isDown ? 32 : 0) +
+                        (キー.BACKSPACE.isDown ? 64 : 0) +
+                        (キー.ESC.isDown ? 128 : 0);
+            return button;
+        }
+
+        // 背景リピート
     }
 
     moveMapGroup(x, y) {
-        var sprint = cursors.shift.isDown ? 4 : 1
+        var sprint = カーソル.shift.isDown ? 4 : 1
         mapX += x * sprint
         mapY += y * sprint
         fieldMap.setXY(mapX, mapY)
@@ -104,11 +119,12 @@ class MainScene extends Phaser.Scene {
 
     update() {
         
-        var button = (cursors.left.isDown ? 1 : 0) + 
-                     (cursors.right.isDown ? 2 : 0) + 
-                     (cursors.up.isDown ? 4 : 0) + 
-                     (cursors.down.isDown ? 8 : 0);
-
+        // ボタン操作
+        // var button = (cursors.left.isDown ? 1 : 0) + 
+        //              (cursors.right.isDown ? 2 : 0) + 
+        //              (cursors.up.isDown ? 4 : 0) + 
+        //              (cursors.down.isDown ? 8 : 0);
+        var button = input();
         if ((button & 1<<0)>0) this.moveMapGroup(spd, 0);
         if ((button & 1<<1)>0) this.moveMapGroup(-spd, 0);
         if ((button & 1<<2)>0) this.moveMapGroup(0, spd);
