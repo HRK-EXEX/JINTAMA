4659af2fbfb1bd6f2796ab7633e4fda994cb4f84
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-10-28 13:02:06 +0900

    ループコミット

--- src/js/G-3/G3-1-maingame.js
+++ src/js/G-3/G3-1-maingame.js
@@ -1,4 +1,3 @@
-
 let fieldMap;
 
 const layerNames = [
@@ -17,13 +16,15 @@ const tilemapNames = [
 ]
 
 // 変数名定義
-var カーソル;
-var キー;
+var cursor;
+var key;
+var debugInfo;
+var currentPlayer;
 
 // 関数名定義
 var input;
-
-var mapX = 0, mapY = 0, scale = 4, spd = 10;
+var loopMap;
+var mapX = 0, mapY = 0, scale = 3, spd = 10;
 
 var toRadian = (degrees) => {
     return degrees * Math.PI / 180;
@@ -44,6 +45,7 @@ class MainScene extends Phaser.Scene {
         this.load.image('baseTileImage', '/map/mapchip2/MapChip/base.png')
         this.load.image('dirtTileImage', '/map/mapchip2/MapChip/tuti1.png')
         this.load.image('dirtTileImage2', '/map/mapchip2/MapChip/tuti2.png')
+        this.load.image('loopTile', '/map/loops/forestLoop.png')
         
         this.load.tilemapTiledJSON('map', '/src/js/G-3/map-data/second-map.json')
     }
@@ -60,6 +62,7 @@ class MainScene extends Phaser.Scene {
         // addTilesetImageの第一引数は、Tiledで使ったタイルセットの名前。
         // 第二引数は、preload()関数で読み込んだ画像のキーである。
 
+        // 関連するタイルセットの名前を定義
         const relatedTileSet = [
             baseTileSet,
             baseTileSet,
@@ -72,9 +75,17 @@ class MainScene extends Phaser.Scene {
         fieldMap = this.add.group()
 
         // 位置の初期化
-        scale = 4;
-        mapX = -8 * 30 * scale;
-        mapY = -8 * 72 * scale;
+        mapX = -8 * 23.4 * scale;
+        mapY = -8 * 69 * scale;
+        
+        // 画面中央のX座標
+        const worldX = this.game.config.width;
+        // 画面中央のY座標
+        const worldY = this.game.config.height;
+        
+        // 背景リピート
+        loopMap = this.add.tileSprite(4 * scale, 4 * scale, worldX, worldY, "loopTile");
+        loopMap.setScale(scale, scale);
 
         // レイヤーを追加
         for(let i=0; i<layerNames.length; i++) {
@@ -84,8 +95,8 @@ class MainScene extends Phaser.Scene {
         }
 
         // 入力のハンドリング
-        カーソル = this.input.keyboard.createCursorKeys()
-        キー = this.input.keyboard.addKeys(
+        cursor = this.input.keyboard.createCursorKeys()
+        key = this.input.keyboard.addKeys(
             {
                 'SPACE': Phaser.Input.Keyboard.KeyCodes.SPACE,
                 'ENTER': Phaser.Input.Keyboard.KeyCodes.ENTER,
@@ -96,49 +107,55 @@ class MainScene extends Phaser.Scene {
 
         input = function input() {
             // ボタン操作
-            var button = (カーソル.left.isDown ? 1 : 0) + 
-                        (カーソル.right.isDown ? 2 : 0) + 
-                        (カーソル.up.isDown ? 4 : 0) + 
-                        (カーソル.down.isDown ? 8 : 0) +
-                        (キー.SPACE.isDown ? 16 : 0) +
-                        (キー.ENTER.isDown ? 32 : 0) +
-                        (キー.BACKSPACE.isDown ? 64 : 0) +
-                        (キー.ESC.isDown ? 128 : 0);
-            return button;
+            var button = (cursor.left.isDown    ? 1<<0 : 0) + 
+                         (cursor.right.isDown   ? 1<<1 : 0) + 
+                         (cursor.up.isDown      ? 1<<2 : 0) + 
+                         (cursor.down.isDown    ? 1<<3 : 0) +
+                         (cursor.shift.isDown   ? 1<<4 : 0) +
+                         (key.SPACE.isDown      ? 1<<5 : 0) +
+                         (key.ENTER.isDown      ? 1<<6 : 0) +
+                         (key.BACKSPACE.isDown  ? 1<<7 : 0) +
+                         (key.ESC.isDown        ? 1<<8 : 0);
+            return Number(button);
         }
 
-        // 背景リピート
+        debugInfo = this.add.text(0, 0, 'Hello World', { fontFamily: 'serif' })
     }
 
     moveMapGroup(x, y) {
-        var sprint = カーソル.shift.isDown ? 4 : 1
-        mapX += x * sprint
-        mapY += y * sprint
+        mapX += x; mapY += y
         fieldMap.setXY(mapX, mapY)
+        loopMap.setTilePosition(-mapX / scale - 4 * scale, -mapY / scale - 8 * scale)
     }
 
     update() {
-        
         // ボタン操作
         // var button = (cursors.left.isDown ? 1 : 0) + 
         //              (cursors.right.isDown ? 2 : 0) + 
         //              (cursors.up.isDown ? 4 : 0) + 
         //              (cursors.down.isDown ? 8 : 0);
         var button = input();
-        if ((button & 1<<0)>0) this.moveMapGroup(spd, 0);
-        if ((button & 1<<1)>0) this.moveMapGroup(-spd, 0);
-        if ((button & 1<<2)>0) this.moveMapGroup(0, spd);
-        if ((button & 1<<3)>0) this.moveMapGroup(0, -spd);
+        var sprint = cursor.shift.isDown ? 4 : 1;
+        if ((button & 1<<0)>0) this.moveMapGroup(spd * sprint, 0);
+        if ((button & 1<<1)>0) this.moveMapGroup(-spd * sprint, 0);
+        if ((button & 1<<2)>0) this.moveMapGroup(0, spd * sprint);
+        if ((button & 1<<3)>0) this.moveMapGroup(0, -spd * sprint);
 
+        debugInfo.setText(button)
         // console.log(button);
     }
 }
 
+const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
+const DEFAULT_HEIGHT = 720 // any height you want
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT
+
 //ゲームに関する設定
 const CONFIG = {
+    mode: Phaser.Scale.FIT,
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT,
     scene: MainScene,
     antialias: false
 }
