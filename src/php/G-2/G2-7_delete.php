2ccd2e6b4141a296da4385c5f6f6e268d205055b
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-10-29 14:46:59 +0900

    delete

--- src/php/G-2/G2-7_delete.php
+++ src/php/G-2/G2-7_delete.php
@@ -3,11 +3,27 @@ session_start();
 require '../db.php';
 $userid = $_SESSION['User']['user_id'];
 
-$roomcheck=$db->prepare("SELECT COUNT(*) FROM Room WHERE room_user1 = ? OR room_user2 = ? OR room_user3 = ? OR room_user4=?");
-$roomcheck->execute([$userid]);
-$count=$roomcheck->fetchColumn();
 
-if($count>0){
+$roomCheck1 = $db->prepare("SELECT COUNT(*) FROM Room WHERE room_user1 = ?");
+$roomCheck1->execute([$userid]);
+$count1 = $roomCheck1->fetchColumn();
+
+
+$roomCheck2 = $db->prepare("SELECT COUNT(*) FROM Room WHERE room_user2 = ?");
+$roomCheck2->execute([$userid]);
+$count2 = $roomCheck2->fetchColumn();
+
+
+$roomCheck3 = $db->prepare("SELECT COUNT(*) FROM Room WHERE room_user3 = ?");
+$roomCheck3->execute([$userid]);
+$count3 = $roomCheck3->fetchColumn();
+
+
+$roomCheck4 = $db->prepare("SELECT COUNT(*) FROM Room WHERE room_user4 = ?");
+$roomCheck4->execute([$userid]);
+$count4 = $roomCheck4->fetchColumn();
+
+if ($count1 > 0 || $count2 > 0 || $count3 > 0){
     $_SESSION['error_message']="このユーザーはルームに参加しているため、アカウントを削除できません。";
     header("Location:/kansho/JINTAMA/src/php/G-2/G2-7.php");
     exit;
