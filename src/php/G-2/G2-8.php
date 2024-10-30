739affc7e2675418217a9e582d766a3272bad302
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-10-30 10:05:21 +0900

    a

---
+++ src/php/G-2/G2-8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/kansho/JINTAMA/src/css/G-2/G2-8.css">
+    <link rel="stylesheet" href="/kansho/JINTAMA/src/css/base/dot_font.css">
+    <title>アカウント削除確認画面</title>
+</head>
+<body>
+    <div class="main">
+       <h1 class="text">本当に削除しますか？</h1>
+    <div class="command">
+       <button class="delete">削除</button>
+       <button onclick="location.href='/kansho/JINTAMA/src/php/G-2/G2-7_setting.html'">戻る</button>
+    </div>
+    </div>
+</body>
+</html>
