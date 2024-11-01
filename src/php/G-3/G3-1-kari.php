3febf56e019ed4e112e039bbcbfce2287812951a
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-11-01 10:03:34 +0900

    n

---
+++ src/php/G-3/G3-1-kari.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>カリカリ</title>
+</head>
+<body>
+    <button onclick="location.href='/kansho/JINTAMA/src/php/G-2/G3-2.php'">ゲーム終了</button>
+</body>
+</html>
