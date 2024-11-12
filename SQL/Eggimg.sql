3bbe3bd85afa46a36d83dedc5c8a5791ad0f801b
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2024-11-12 14:21:20 +0900

    d

---
+++ SQL/Eggimg.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Eggimg (
+    eggimg_id int(10) AUTO_INCREMENT,
+    img_pass varchar(100),
+    primary key(eggimg_id)
+);
+
+INSERT INTO `Eggimg`(`eggimg_id`, `img_pass`) VALUES (null,'');
--- SQL/InitState.sql
+++ SQL/InitState.sql
@@ -9,3 +9,15 @@ CREATE TABLE InitState(
     PRIMARY KEY (pattern_id),
     FOREIGN KEY (skill_id) REFERENCES Skills (skill_id)
 );
+
+
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,1,10,10,1,19,'bakemon.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,2,10,7,16,17,'fukurou.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,3,10,12,9,9,'ikari.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,4,10,10,10,10,'kaeru.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,5,10,15,10,5,'koara.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,6,10,5,10,15,'melondog.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,7,10,7,15,8,'obake.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,8,10,5,10,15,'obebu.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,9,10,18,8,4,'takugorira.png');
+INSERT INTO `InitState`(`pattern_id`, `skill_id`, `score`, `hp`, `charm`, `sense`, `img`) VALUES (null,10,10,6,11,14,'kaba.png');
