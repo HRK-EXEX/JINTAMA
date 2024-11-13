793b8f4da4de294166b91be4c5c32f106b907d50
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-13 10:27:11 +0900

    phpのほうを更新

--- src/php/G-3/G3-1-maingame.php
+++ src/php/G-3/G3-1-maingame.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/src/css/G-3/G3-1-game.css">
-    <link rel="stylesheet"  href="/src/css/base/dot_font.css" /> 
+    <link rel="stylesheet" href="/kansho/JINTAMA/src/css/G-3/G3-1-game.css">
+    <link rel="stylesheet"  href="/kansho/JINTAMA/src/css/base/dot_font.css" /> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/phaser@v3.85.2/dist/phaser.min.js"></script>
-    <script src="/src/js/G-3/initialize.js" type="module"></script>
-    <script src="/src/js/G-3/gameBoard.js" type="module"></script>
-    <script src="/src/js/G-3/dialogSelectBox.js" type="module"></script>
-    <script src="/src/js/G-3/main.js" type="module"></script>
+    <script src="/kansho/JINTAMA/src/js/G-3/initialize.js" type="module"></script>
+    <script src="/kansho/JINTAMA/src/js/G-3/gameBoard.js" type="module"></script>
+    <script src="/kansho/JINTAMA/src/js/G-3/dialogSelectBox.js" type="module"></script>
+    <script src="/kansho/JINTAMA/src/js/G-3/main.js" type="module"></script>
     <title>メインゲーム</title>
 </head>
 <body>
