7e8633c882018bb0ccf1ed086fb0e56209b5e92e
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-11-18 12:06:21 +0900

    a

--- src/js/G-3/form.js
+++ src/js/G-3/form.js
@@ -1,5 +1,5 @@
-import { player } from "./initialize";
-document.getElementById("user1").value = player[0];
-document.getElementById("user2").value = player[1];
-document.getElementById("user3").value = player[2];
-document.getElementById("user4").value = player[3];
+ import { player } from "./initialize";
+document.getElementById("user1").value = JSON.stringify (player[0]);
+document.getElementById("user2").value = JSON.stringify (player[1]);
+document.getElementById("user3").value = JSON.stringify (player[2]);
+document.getElementById("user4").value = JSON.stringify (player[3]);
