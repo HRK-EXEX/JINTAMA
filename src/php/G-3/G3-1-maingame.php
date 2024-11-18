e030a9039afa9242c09e2bdbe2bb901258e95947
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-18 10:21:19 +0900

    pjj

--- src/php/G-3/G3-1-maingame.php
+++ src/php/G-3/G3-1-maingame.php
@@ -1,18 +1,16 @@
 <?php
     session_start();
-    var_dump($_SESSION);
+    // var_dump($_SESSION);
     $roomId = $_SESSION['User']['room_id'];
 
     require "../db.php";
     $sql = $db -> query( "SELECT * FROM room");
-    $result=$sql ->fetchAll();
-    
+    $result = $sql -> fetchAll();
 
     echo "<div id='username1' style='display: none;'>".(isset($_SESSION['User1']) ? implode(",", $_SESSION['User1']) : "empty")."</div><br>";
-    if(isset($result['Room']['room_user2']))echo "<div id='username2' style='display: none;'>".(isset($_SESSION['User2']) ? implode(",", $_SESSION['User2']) : "empty")."</div><br>";
-    if(isset($result['Room']['room_user3']))echo "<div id='username3' style='display: none;'>".(isset($_SESSION['User3']) ? implode(",", $_SESSION['User3']) : "empty")."</div><br>";
-    if(isset($result['Room']['room_user4']))echo "<div id='username4' style='display: none;'>".(isset($_SESSION['User4']) ? implode(",", $_SESSION['User4']) : "empty")."</div><br>";
-    
+    if (isset($result['Room']['room_user2'])) echo "<div id='username2' style='display: none;'>".(isset($_SESSION['User2']) ? implode(",", $_SESSION['User2']) : "empty")."</div><br>";
+    if (isset($result['Room']['room_user3'])) echo "<div id='username3' style='display: none;'>".(isset($_SESSION['User3']) ? implode(",", $_SESSION['User3']) : "empty")."</div><br>";
+    if (isset($result['Room']['room_user4'])) echo "<div id='username4' style='display: none;'>".(isset($_SESSION['User4']) ? implode(",", $_SESSION['User4']) : "empty")."</div><br>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
