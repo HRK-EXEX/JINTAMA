dfab3d98d257b57f3a9f2e2c1740c6c5ca7a20ad
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-11-18 10:16:08 +0900

    a

--- src/php/G-3/G3-1-maingame.php
+++ src/php/G-3/G3-1-maingame.php
@@ -9,9 +9,9 @@
     
 
     echo "<div id='username1' style='display: none;'>".(isset($_SESSION['User1']) ? implode(",", $_SESSION['User1']) : "empty")."</div><br>";
-    if(isset($result['User']['room_user2']))echo "<div id='username2' style='display: none;'>".(isset($_SESSION['User2']) ? implode(",", $_SESSION['User2']) : "empty")."</div><br>";
-    if(isset($result['User']['room_user3']))echo "<div id='username3' style='display: none;'>".(isset($_SESSION['User3']) ? implode(",", $_SESSION['User3']) : "empty")."</div><br>";
-    if(isset($result['User']['room_user4']))echo "<div id='username4' style='display: none;'>".(isset($_SESSION['User4']) ? implode(",", $_SESSION['User4']) : "empty")."</div><br>";
+    if(isset($result['Room']['room_user2']))echo "<div id='username2' style='display: none;'>".(isset($_SESSION['User2']) ? implode(",", $_SESSION['User2']) : "empty")."</div><br>";
+    if(isset($result['Room']['room_user3']))echo "<div id='username3' style='display: none;'>".(isset($_SESSION['User3']) ? implode(",", $_SESSION['User3']) : "empty")."</div><br>";
+    if(isset($result['Room']['room_user4']))echo "<div id='username4' style='display: none;'>".(isset($_SESSION['User4']) ? implode(",", $_SESSION['User4']) : "empty")."</div><br>";
     
 ?>
 <!DOCTYPE html>
