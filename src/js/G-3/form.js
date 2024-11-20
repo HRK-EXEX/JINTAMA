1677f276f6ab5e406c1afc28bfd716a03df0ed05
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-20 10:43:40 +0900

    あ

--- src/js/G-3/form.js
+++ src/js/G-3/form.js
@@ -48,7 +48,7 @@ export function changeForm(players) {
 
             userElement = document.getElementById(`user${index + 1}`);
             if (userElement) {
-                userElement.value = JSON.stringify(json, getCircularReplacer()); // 各ユーザーのデータを埋め込む
+                userElement.value.innerHTML = JSON.stringify(json, getCircularReplacer()); // 各ユーザーのデータを埋め込む
             } else console.log("userElement is undefined");
         }
 
