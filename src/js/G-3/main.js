5872fe2049be62daa16e4676eb374f89aa230733
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-22 10:29:17 +0900

    null?

--- src/js/G-3/main.js
+++ src/js/G-3/main.js
@@ -4,6 +4,7 @@ const DEFAULT_WIDTH = window.innerWidth
 const DEFAULT_HEIGHT = window.innerHeight // any height you want
 
 export var playerData = JSON.parse(document.getElementById("users").innerHTML);
+console.log(playerData);
 
 //ゲームに関する設定
 const CONFIG = {
