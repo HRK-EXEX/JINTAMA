71d5391378aae7db529b380df0769270bfebe23b
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-22 10:36:22 +0900

    use textContent instead innerHTML

--- src/js/G-3/main.js
+++ src/js/G-3/main.js
@@ -3,7 +3,7 @@ import { MainScene } from "./mainScene.js";
 const DEFAULT_WIDTH = window.innerWidth
 const DEFAULT_HEIGHT = window.innerHeight // any height you want
 
-export var playerData = JSON.parse(document.getElementById("users").innerHTML.slice(1,-1));
+export var playerData = JSON.parse(document.getElementById("users").textContent);
 console.log(playerData);
 
 //ゲームに関する設定
