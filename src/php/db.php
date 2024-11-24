e6c9989efa792cddf00e7336c6ea1e1e3eb5ff96
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2024-11-25 02:48:00 +0900

    仮想環境・本番環境で対応可能に

--- src/php/db.php
+++ src/php/db.php
@@ -1,10 +1,22 @@
 <?php
-    const SERVER = "mysql309.phy.lolipop.lan";
-    const DBNAME = "LAA1517439-kansho";
-    const USER = "LAA1517439";
-    const PASS = "Pass1234";
-    const DBINFO = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
-    $db = new PDO(DBINFO, USER, PASS);
+    var $db; // db用変数
+    if ($_SERVER['SERVER_NAME'] == 'localhost') {
+        // 仮想環境
+        const SERVER = "localhost";
+        const DBNAME = "test";
+        const USER = "root";
+        const PASS = "";
+        const DBINFO = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+        $db = new PDO(DBINFO, USER, PASS);
+    } else {
+        // 本番環境
+        const SERVER = "mysql309.phy.lolipop.lan";
+        const DBNAME = "LAA1517439-kansho";
+        const USER = "LAA1517439";
+        const PASS = "Pass1234";
+        const DBINFO = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+        $db = new PDO(DBINFO, USER, PASS);
+    }
 
 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
